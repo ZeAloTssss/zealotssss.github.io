45c5c488466d73c884ef531f5614bd0468c2ac34
ZeAloTssss <ki7looov@gmail.com>
2020-03-26 20:12:43 +0300

    1

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('formyprogtests@gmail.com'); // от кого будет уходить письмо?
-$mail->addAddress('kdeeenk@gmail.com');     // Кому будет уходить письмо 
+$mail->addAddress('jamatot171@svpmail.com');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
