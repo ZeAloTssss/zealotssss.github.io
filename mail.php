e3fb1ba91303a98db5a6d7c58f01395d3ec3e922
ZeAloTssss <ki7looov@gmail.com>
2020-03-26 20:09:15 +0300

    1

--- mail.php
+++ mail.php
@@ -11,12 +11,12 @@ $email = $_POST['user_email'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.google.com';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'formyprogtests@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = '#asdf#asdf'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('formyprogtests@gmail.com'); // от кого будет уходить письмо?
 $mail->addAddress('kdeeenk@gmail.com');     // Кому будет уходить письмо 
